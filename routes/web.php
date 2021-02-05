a262db1e670ea1d9162800a8dba55dee1e71d574
ulissesms <ti03@umuprev.com.br>
2021-02-05 16:24:59 -0300

    usando e validando parametros no route

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,12 @@ Route::get('/sobre-nos','SobreNosController@index');
 
 Route::get('/contato', 'ContatoController@index');
 
-Route::get('/contato/{nome}',function(string $nome){
-    echo "Estamos aqui $nome";
-});
+Route::get('/contato/{nome}/{categoria_id}',
+function(
+    string $nome = 'Desconhecido',
+    int $categoria_id  =1
+){
+    echo "Estamos aqui $nome -$categoria_id";
+})-> where(
+    'categoria_id','[0-9]+'
+);
