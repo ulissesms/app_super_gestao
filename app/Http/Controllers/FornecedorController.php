2e00c6892c27594f59ae65f78105f3d45955df01
ulissesms <ti03@umuprev.com.br>
2021-02-08 12:59:37 -0300

    trabalhando com if e else no blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor.index');
+        $fornecedor = ['fornecedor 1','fornecedor 2','fornecedor 3'];
+        return view('app.fornecedor.index',compact('fornecedor'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,12 @@
 <h3>Fornecedores</h3>
 
 {{-- Trabalhando com o blade --}}
+
+
+@if(count($fornecedor)> 0 && count($fornecedor)<10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+    @elseif(count($fornecedor)>10)
+    <h3>Existem varios fornecedores cadastrados</h3>
+    @else
+    <h3>Ainda nao existem fornecedores cadastradoos</h3>
+@endif
